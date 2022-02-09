f761acc8fcea6d77e4d3fbf4d65910ff0b306334
Juan Mrad <juansmrad@gmail.com>
2022-02-08 19:08:07 -0600

    tic tac toe solution

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,32 +1,95 @@
 var content = document.getElementById('content');
 
-var ticTacToe = "Replace this with your own abstraction of Tic Tac Toe"
+var board = [
+    ["-", "-", "-"], // 0
+    ["-", "-", "-"], // 1
+    ["-", "-", "-"] // 2
+];
+
+var ticTacToe = {
+    board: board,
+    currentPlayer: "X"
+}
 
 content.innerHTML = renderGame(ticTacToe);
 
+function playGame(row, column) {
+    console.log(`clicked play game on: ${row} ${column}`);
+    console.log(ticTacToe.board[row][column]);
 
+    // checking the position wasn't played already.
+    if (ticTacToe.board[row][column] != '-') {
+        alert('That position was already selected');
+        return;
+    }
 
+    // changing the board with the position selected.
+    ticTacToe.board[row][column] = ticTacToe.currentPlayer;
+
+    // check if someone won.
+    // 0,0 == 1,0  AND 1,0 == 2,0
+    if (ticTacToe.board[0][0] != '-'
+        && ticTacToe.board[0][0] == ticTacToe.board[1][0]
+        && ticTacToe.board[1][0] == ticTacToe.board[2][0]) {
+        declareWinner();
+    } else if (ticTacToe.board[0][1] != '-'
+        && ticTacToe.board[0][1] == ticTacToe.board[1][1]
+        && ticTacToe.board[1][1] == ticTacToe.board[2][1]) {
+        declareWinner();
+    } else if (ticTacToe.board[0][2] != '-'
+        && ticTacToe.board[0][2] == ticTacToe.board[1][2]
+        && ticTacToe.board[1][2] == ticTacToe.board[2][2]) {
+        declareWinner();
+    } else if (ticTacToe.board[0][0] != '-'
+        && ticTacToe.board[0][0] == ticTacToe.board[0][1]
+        && ticTacToe.board[0][1] == ticTacToe.board[0][2]) {
+        declareWinner();
+    } else if (ticTacToe.board[1][0] != '-'
+        && ticTacToe.board[1][0] == ticTacToe.board[1][1]
+        && ticTacToe.board[1][1] == ticTacToe.board[1][2]) {
+        declareWinner();
+    }
+
+    // chaning the current player.
+    if (ticTacToe.currentPlayer == 'X') {
+        ticTacToe.currentPlayer = 'O';
+    } else {
+        ticTacToe.currentPlayer = 'X';
+    }
+
+    // re rendering the page
+    content.innerHTML = renderGame(ticTacToe);
+}
+
+function declareWinner() {
+    alert(`Player: ${ticTacToe.currentPlayer} Won!`)
+    ticTacToe.board = [
+        ["-", "-", "-"], // 0
+        ["-", "-", "-"], // 1
+        ["-", "-", "-"]  // 2
+    ];
+}
 
 function renderGame(game) {
     // Change this render function to use the "game" parameter
 
     return `
         <div class="container d-flex flex-column justify-content-start align-items-center">
-            <h4>It's player O's turn!</h4>
-            <div class="w-50 text-center">
-                <button>X</button>
-                <button>O</button>
-                <button>X</button>
+            <h4>It's player ${game.currentPlayer}'s turn!</h4>
+            <div class="w-50 text-center"> <!-- 0 -->
+                <button onclick="playGame(0,0)">${game.board[0][0]}</button>
+                <button onclick="playGame(0,1)">${game.board[0][1]}</button>
+                <button onclick="playGame(0,2)">${game.board[0][2]}</button>
             </div>
-            <div class="w-50 text-center">
-                <button>-</button>
-                <button>-</button>
-                <button>-</button>
+            <div class="w-50 text-center">  <!-- 1 -->
+                <button onclick="playGame(1,0)">${game.board[1][0]}</button>
+                <button onclick="playGame(1,1)">${game.board[1][1]}</button>
+                <button onclick="playGame(1,2)">${game.board[1][2]}</button>
             </div>
-            <div class="w-50 text-center">
-                <button>O</button>
-                <button>-</button>
-                <button>X</button>
+            <div class="w-50 text-center">  <!-- 2 -->
+                <button onclick="playGame(2,0)">${game.board[2][0]}</button>
+                <button onclick="playGame(2,1)">${game.board[2][1]}</button>
+                <button onclick="playGame(2,2)">${game.board[2][2]}</button>
             </div>
         </div>
     `
